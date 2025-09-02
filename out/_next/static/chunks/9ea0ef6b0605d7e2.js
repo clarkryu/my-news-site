b7f55de8c8caaa13e672f761845d5a457d5e52e5
clark ryu <ajoojoa76@gmail.com>
2025-09-02 23:32:39 +0900

    정적 빌드 완료: out 폴더 추가

---
+++ out/_next/static/chunks/9ea0ef6b0605d7e2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/d41a05212121dd76.js",
+  "static/chunks/turbopack-6afd834409410eac.js"
+])
---
+++ out/_next/static/chunks/c8ccbeb7801d1a18.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/d41a05212121dd76.js",
+  "static/chunks/turbopack-67faa688792ad5f0.js"
+])
